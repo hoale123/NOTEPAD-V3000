c275ef15c2abf6e85e983d8eb7e7d4aab1cdef4f
Hoa Le <Hoa_le9485@yahoo.com>
2022-01-22 16:48:02 -0500

    gg

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,13 +3,6 @@ import NoteEditor from "./NoteEditor";
 import NoteViewer from "./NoteViewer";
 import Instructions from "./Instructions";
 
-/*
-  Advice: If you cannot figure out how to get this component to work,
-          move the div and getContent up into NoteContainer and
-          try to get it to work in the parent first.
-          Then complete the rest of your app before attempting to
-          refactor to get this Content component to work.
-*/
 function Content({selectedNotes,toggleIsEdit,isEdit,isSelected}) {
 
   const toggleEdit = () =>{
